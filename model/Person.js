08988e6001bd9bbafb689f69183bf11605c01106
angusking47 <angusking47@link.cuhk.edu.hk>
2022-07-31 00:33:00 +0800

    phonebook

--- model/Person.js
+++ model/Person.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 require('dotenv').config()
 
-const PersonSchema = new mongoose.schema({
-    name:String,
-    number:String
+const PersonSchema = new mongoose.Schema({
+    name: String,
+    number: String,
 })
 
 module.exports = mongoose.model('Person', PersonSchema)
